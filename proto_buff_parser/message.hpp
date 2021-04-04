7e7ba46d257469d9839a2185e9fb448e90c276a4
Viktor Repin <hiddenstmail@gmail.com>
2021-04-04 17:56:22 +0700

    Add fields_iterator

--- proto_buff_parser/message.hpp
+++ proto_buff_parser/message.hpp
@@ -4,25 +4,29 @@
 #include <string>
 #include <vector>
 
-#include <proto_buff_parser/field.hpp>
 #include <proto_buff_parser/file.hpp>
 #include <proto_buff_parser/package.hpp>
+#include <proto_buff_parser/container/fields_container.hpp>
 
 namespace proto_buff_parser {
 
 class Message {
  public:
   explicit Message(const std::string& name, const File& file, const Package& package,
+                   const container::FieldsContainer& fields_container,
                    const Message& parent_message);
-  explicit Message(const std::string& name, const File& file, const Package& package);
+  explicit Message(const std::string& name, const File& file, const Package& package,
+                   const container::FieldsContainer& fields_container);
 
   const Package& GetPackage() const noexcept;
   const File& GetFile() const noexcept;
   const Message& GetParentMessage() const noexcept;
+  iterator::FieldsIterator& GetFieldsIterator();
 
  private:
   std::string name_;
   const Message& parent_message_;
+  const container::FieldsContainer& fields_container_;
   const File& file_;
   const Package& package_;
 };
