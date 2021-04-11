dee7d843f1fb7f0db8b98e227a6c8a910e801c79
Viktor Repin <hiddenstmail@gmail.com>
2021-04-12 00:42:25 +0700

    Refactor parameter name

--- protobuf_parser/protobuf_parser.hpp
+++ protobuf_parser/protobuf_parser.hpp
@@ -17,27 +17,27 @@ template <typename Serializer>
 class ProtobufParser {
  public:
   ProtobufParser() = delete;
-  explicit ProtobufParser(const std::filesystem::path& path);
+  explicit ProtobufParser(const std::filesystem::path& root_path);
 
-  std::string SerializeDirectory(const std::filesystem::path& dir_path);
+  std::string SerializeDirectory(const std::filesystem::path& relative_to_root_directory_path);
   std::string SerializePackage(const std::string& package_name);
 
  private:
   std::string GetPathRelativeToRootDirectory(const std::string& full_path);
   google::protobuf::compiler::DiskSourceTree disk_source_tree_;
   StubMultipleErrorCollector error_collector_;
-  std::filesystem::path path_;
+  std::filesystem::path root_path_;
   Serializer serializer_;
   ProtobufStorage storage_;
 };
 
 template <typename Serializer>
-ProtobufParser<Serializer>::ProtobufParser(const std::filesystem::path& path) : path_(path) {
-  // Add empty string argument as virtual_path parameter to indicate DiskSourceTree to map path_ as
+ProtobufParser<Serializer>::ProtobufParser(const std::filesystem::path& root_path) : root_path_(root_path) {
+  // Add empty string argument as virtual_path parameter to indicate DiskSourceTree to map root_path_ as
   // a root
-  disk_source_tree_.MapPath(std::string{}, path_.string());
+  disk_source_tree_.MapPath(std::string{}, root_path_.string());
   google::protobuf::compiler::Importer importer{&disk_source_tree_, &error_collector_};
-  std::filesystem::recursive_directory_iterator recursive_directory_iterator{path_};
+  std::filesystem::recursive_directory_iterator recursive_directory_iterator{root_path_};
 
   std::set<std::string> directories_names;
   std::set<std::string> packages_names;
@@ -57,8 +57,8 @@ ProtobufParser<Serializer>::ProtobufParser(const std::filesystem::path& path) : 
 }
 
 template <typename Serializer>
-std::string ProtobufParser<Serializer>::SerializeDirectory(const std::filesystem::path& dir_path) {
-  auto* directory = storage_.FindDirectory(dir_path.string());
+std::string ProtobufParser<Serializer>::SerializeDirectory(const std::filesystem::path& relative_to_root_directory_path) {
+  auto* directory = storage_.FindDirectory(relative_to_root_directory_path.string());
   view::View<Directory, Serializer> directory_view{directory, storage_, serializer_};
   return directory_view.Serialize();
 }
@@ -75,7 +75,7 @@ std::string ProtobufParser<Serializer>::GetPathRelativeToRootDirectory(
     const std::string& full_path) {
   std::string changed_path{};
   std::size_t i = 0;
-  std::string root_path = path_.string();
+  std::string root_path = root_path_.string();
   while (full_path[i] == root_path[i]) {
     ++i;
   }
