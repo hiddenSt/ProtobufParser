62223702a0dc0203e68cdd47c6d1e65dba9fe1d4
Viktor Repin <hiddenstmail@gmail.com>
2021-04-12 19:50:36 +0700

    Refactor tests

--- tests/protobuf_parser_tests.cpp
+++ tests/protobuf_parser_tests.cpp
@@ -5,37 +5,39 @@
 #include <protobuf_parser/protobuf_parser.hpp>
 #include <protobuf_parser/serializers/json_serializer.hpp>
 
-namespace tests {
-TEST(ProtobufParserTests, CanCreateProtobufParser) {
+class ProtobufParserTests : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    root_path_ = new std::filesystem::path{path_to_tests_proto_directory_};
+  }
+
   std::string path_to_tests_proto_directory_ = "../../tests";
-  std::filesystem::path root_path{path_to_tests_proto_directory_};
+  std::filesystem::path* root_path_;
+};
+
+namespace tests {
+TEST_F(ProtobufParserTests, CanCreateProtobufParser) {
   ASSERT_NO_THROW(
-      protobuf_parser::ProtobufParser<protobuf_parser::serializer::JsonSerializer>{root_path});
+      protobuf_parser::ProtobufParser<protobuf_parser::serializer::JsonSerializer>{*root_path_});
 }
 
-TEST(ProtobufParserTests, CanGetSerializedMessageFromDirectory) {
-  std::string path_to_tests_proto_directory_ = "../../tests";
-  std::filesystem::path root_path{path_to_tests_proto_directory_};
+TEST_F(ProtobufParserTests, CanGetSerializedMessageFromDirectory) {
   std::string directory_path{"protos"};
   protobuf_parser::ProtobufParser<protobuf_parser::serializer::JsonSerializer> protobuf_parser{
-      root_path};
+      *root_path_};
   ASSERT_NO_THROW(protobuf_parser.SerializeDirectory(directory_path));
 }
 
-TEST(ProtobufParserTests, CanGetSerializedMessageFromPackage) {
-  std::string path_to_tests_proto_directory_ = "../../tests";
-  std::filesystem::path root_path{path_to_tests_proto_directory_};
+TEST_F(ProtobufParserTests, CanGetSerializedMessageFromPackage) {
   std::string package_name{"test_package"};
   protobuf_parser::ProtobufParser<protobuf_parser::serializer::JsonSerializer> protobuf_parser{
-      root_path};
+      *root_path_};
   ASSERT_NO_THROW(protobuf_parser.SerializePackage(package_name));
 }
 
-TEST(ProtobufParserTests, ReturnsCorrectJson) {
-  std::string path_to_tests_proto_directory_ = "../../tests";
-  std::filesystem::path root_path{path_to_tests_proto_directory_};
+TEST_F(ProtobufParserTests, ReturnsCorrectJson) {
   protobuf_parser::ProtobufParser<protobuf_parser::serializer::JsonSerializer> protobuf_parser{
-      root_path};
+      *root_path_};
   auto message = protobuf_parser.SerializeDirectory("protos");
   ASSERT_EQ(1, 2) << message;
 }
