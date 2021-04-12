fb08ca2cc5528502853ed2aae007d452f6be4fb3
Viktor Repin <hiddenstmail@gmail.com>
2021-04-12 17:54:32 +0700

    Add output for reserved numbers and names

--- protobuf_parser/serializers/json_serializer.cpp
+++ protobuf_parser/serializers/json_serializer.cpp
@@ -12,12 +12,24 @@ void JsonSerializer::AddMessage(const Message& message) {
   message_element["name"] = message.GetName();
   message_element["file"] = message.GetFile()->GetName();
   message_element["fields"] = nlohmann::json::array();
+  message_element["reserved_names"] = nlohmann::json::array();
+  message_element["reserved_numbers"] = nlohmann::json::array();
+
+  for (auto& reserved_name: message.GetReservedNames()) {
+    message_element["reserved_names"].emplace_back(reserved_name);
+  }
+
+  for (auto& reserved_number: message.GetReservedNumbers()) {
+    message_element["reserved_numbers"].emplace_back(reserved_number);
+  }
+
   for (auto& field : message.GetFields()) {
     nlohmann::json field_element;
     field_element["name"] = field.GetName();
     field_element["type"] = field.GetType();
     field_element["number"] = field.GetNumber();
     field_element["optional"] = field.IsOptional();
+    field_element["repeated"] = field.IsRepeated();
     message_element["fields"].emplace_back(field_element);
   }
   json_representation_.emplace_back(message_element);
