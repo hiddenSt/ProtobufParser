46b96e87751f404711fa35b36dc8edd22849e084
Viktor Repin <hiddenstmail@gmail.com>
2021-04-21 16:53:26 +0700

    Fix bug

--- protobuf_parser/view/view.cpp
+++ protobuf_parser/view/view.cpp
@@ -6,7 +6,7 @@ namespace view {
 template <>
 void View<Directory>::EmplaceElementChildren(const Directory* directory, std::queue<const Directory*>& elements_queue) {
   for (auto& dir: storage_->directories_) {
-    if (dir.GetParentDirectory().GetName() == directory->GetName()) {
+    if (dir.HasParent() && dir.GetParentDirectory().GetName() == directory->GetName()) {
       elements_queue.emplace(&dir);
     }
   }
@@ -24,7 +24,7 @@ void View<Directory>::AddElementsMessages(const Directory* directory) {
 template <>
 void View<Package>::EmplaceElementChildren(const Package* package, std::queue<const Package*>& elements_queue) {
   for (auto& a_package: storage_->packages_) {
-    if (a_package.GetParentPackage().GetName() == package->GetName()) {
+    if (a_package.HasParent() && a_package.GetParentPackage().GetName() == package->GetName()) {
       elements_queue.emplace(&a_package);
     }
   }
