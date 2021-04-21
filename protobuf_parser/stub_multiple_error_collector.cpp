6d502070e21a7d3fb688903f2f08df1328b25937
Viktor Repin <hiddenstmail@gmail.com>
2021-04-21 20:03:10 +0700

    Add throw statement to stub_multiple_error_collector

--- protobuf_parser/stub_multiple_error_collector.cpp
+++ protobuf_parser/stub_multiple_error_collector.cpp
@@ -1,9 +1,11 @@
 #include "stub_multiple_error_collector.hpp"
+#include <protobuf_parser/parse_exception.hpp>
 
 namespace protobuf_parser {
 
 void StubMultipleErrorCollector::AddError(const std::string& filename, int line, int column,
                                           const std::string& message) {
+  throw ParseException{filename, line, column, message};
 }
 
 void StubMultipleErrorCollector::AddWarning(const std::string& filename, int line, int column,
