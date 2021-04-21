33f839032080bcb42773158b7924a7af86fb5a77
Viktor Repin <hiddenstmail@gmail.com>
2021-04-21 20:01:20 +0700

    Add ParseException

---
+++ protobuf_parser/parse_exception.cpp
@@ -0,0 +1,19 @@
+#include "parse_exception.hpp"
+
+namespace protobuf_parser {
+
+ParseException::ParseException(const std::string& filename, int line, int column,
+                                                const std::string& message)
+    : filename_(filename),
+      line_(line),
+      column_(column),
+      message_(message) {
+}
+
+const char* ParseException::what() const noexcept {
+  std::string* error_message_ = new std::string{};
+  *error_message_ += "Error in file " + filename_ + " at line " + std::to_string(line_) + " in column " + std::to_string(column_);
+  return error_message_->c_str();
+}
+
+}
