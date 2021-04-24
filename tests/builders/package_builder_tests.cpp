8c4eecb2f9b80ce37ac2a1b08986717a818eeeff
Viktor Repin <hiddenstmail@gmail.com>
2021-04-24 19:59:03 +0700

    Add more PackageBuilder test cases

--- tests/builders/package_builder_tests.cpp
+++ tests/builders/package_builder_tests.cpp
@@ -11,11 +11,68 @@ TEST(PackageBuilderTests, CanCreatePackageBuilderBuilder) {
 TEST(PackageBuilderTests, CanSetUpName) {
   std::string name{"package_name"};
   protobuf_parser::builders::PackageBuilder builder{};
+  ASSERT_NO_THROW(builder.SetUpName(name));
+}
+
+TEST(PackageBuilderTests, CanGetPackageName) {
+  std::string name{"package_name"};
+  protobuf_parser::builders::PackageBuilder builder{};
+  builder.SetUpName(name);
+  ASSERT_NO_THROW(builder.GetName());
+}
+
+TEST(PackageBuilderTests, MethodGetPackageNameReturnsCorrectName) {
+  std::string name{"package_name"};
+  protobuf_parser::builders::PackageBuilder builder{};
   builder.SetUpName(name);
   ASSERT_EQ(builder.GetPackage().GetName(), name);
 }
 
+TEST(PackageBuilderTests, CanGetPackage) {
+  std::string name{"package_name"};
+  protobuf_parser::builders::PackageBuilder builder{};
+  builder.SetUpName(name);
+  ASSERT_NO_THROW(builder.GetPackage());
+}
+
+TEST(PackageBuilderTests, MethodGetPackageReturnsCorrectPackage) {
+  std::string name{"package_name"};
+  protobuf_parser::builders::PackageBuilder builder{};
+  builder.SetUpName(name);
+  auto package = std::move(builder.GetPackage());
+  ASSERT_EQ(package.GetName(), name);
+}
+
 TEST(PackageBuilderTests, CanSetUpParent) {
+  std::string parent_name{"parent_name"};
+  std::string child_name{"child_name"};
+
+  protobuf_parser::builders::PackageBuilder parent_builder{};
+  protobuf_parser::builders::PackageBuilder child_builder{};
+  parent_builder.SetUpName(parent_name);
+  child_builder.SetUpName(child_name);
+
+  auto parent_package = std::move(parent_builder.GetPackage());
+  ASSERT_NO_THROW(child_builder.SetUpParent(&parent_package));
+}
+
+TEST(PackageBuilderTests, CanGetParent) {
+  std::string parent_name{"parent_name"};
+  std::string child_name{"child_name"};
+
+  protobuf_parser::builders::PackageBuilder parent_builder{};
+  protobuf_parser::builders::PackageBuilder child_builder{};
+  parent_builder.SetUpName(parent_name);
+  child_builder.SetUpName(child_name);
+
+  auto parent_package = std::move(parent_builder.GetPackage());
+  child_builder.SetUpParent(&parent_package);
+  auto child_package = std::move(child_builder.GetPackage());
+
+  ASSERT_NO_THROW(&child_package.GetParentPackage());
+}
+
+TEST(PackageBuilderTests, MethodGetParentReturnsCorrectParent) {
   std::string parent_name{"parent_name"};
   std::string child_name{"child_name"};
 
@@ -30,5 +87,26 @@ TEST(PackageBuilderTests, CanSetUpParent) {
 
   ASSERT_EQ(&child_package.GetParentPackage(), &parent_package);
 }
+
+TEST(PackageBuilderTests, MethodIsParentReturnsTrueIfArgumentIsChildPackageName) {
+  std::string parent_name{"parent_name"};
+  std::string child_name{"parent_name.child_name"};
+
+  protobuf_parser::builders::PackageBuilder parent_builder{};
+  parent_builder.SetUpName(parent_name);
+
+  ASSERT_TRUE(parent_builder.IsParent(child_name));
+}
+
+TEST(PackageBuilderTests, MethodIsParentReturnsFalseIfArgumentIsNotChildPackageName) {
+  std::string parent_name{"parent_name"};
+  std::string not_child_name{"not_child_name"};
+
+  protobuf_parser::builders::PackageBuilder parent_builder{};
+  parent_builder.SetUpName(parent_name);
+
+  ASSERT_FALSE(parent_builder.IsParent(not_child_name));
+}
+
 
 }  // namespace tests
