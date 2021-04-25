5c19abd12d0462dd2c43a3922239f57ef6d1760f
Viktor Repin <hiddenstmail@gmail.com>
2021-04-25 10:53:58 +0700

    Add Enum Type

--- protobuf_parser/elements/enum.cpp
+++ protobuf_parser/elements/enum.cpp
@@ -1,5 +1,24 @@
-//
-// Created by hiddenst on 22.04.2021.
-//
-
 #include "enum.hpp"
+
+namespace protobuf_parser {
+
+bool Enum::HasParentMessage() const noexcept {
+  return parent_message_ != nullptr;
+}
+
+const Message& Enum::GetParentMessage() const {
+  return *parent_message_;
+}
+
+const File& Enum::GetFile() const {
+  return *file_;
+}
+
+const std::string& Enum::GetName() const {
+  return name_;
+}
+
+const std::map<std::size_t, std::string>& Enum::GetValues() const {
+  return values_;
+}
+}
--- protobuf_parser/elements/enum.hpp
+++ protobuf_parser/elements/enum.hpp
@@ -1,20 +1,32 @@
 #ifndef PROTOBUF_PARSER_PROTOBUF_PARSER_ELEMENTS_ENUM_HPP_
 #define PROTOBUF_PARSER_PROTOBUF_PARSER_ELEMENTS_ENUM_HPP_
 
-#include <protobuf_parser/elements/element.hpp>
 #include <vector>
 #include <string>
+#include <map>
+
+#include <protobuf_parser/elements/element.hpp>
+#include <protobuf_parser/elements/file.hpp>
+#include <protobuf_parser/elements/message.hpp>
 
 namespace protobuf_parser {
 
 class Enum : public Element {
  public:
   Enum() = default;
-  const std::vector<std::string>& GetValues() const;
+
+  bool HasParentMessage() const noexcept;
+  const Message& GetParentMessage() const;
+  const File& GetFile() const;
+  const std::string& GetName() const;
+  const std::map<std::size_t, std::string>& GetValues() const;
+  
  private:
-  std::vector<std::string> values_;
+  std::string name_;
+  std::map<std::size_t, std::string> values_;
   std::vector<std::size_t> reserved_numbers_;
-  Element* parent_;
+  File* file_;
+  Message* parent_message_;
 };
 
 }
